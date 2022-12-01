676cd4c7900dc1e8d59ffa3a01857982870077b1
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2022-12-01 08:07:34 -0600

    Update schedule.py

--- src/dagster_data_lake/dagster_data_lake/schedules/schedule.py
+++ src/dagster_data_lake/dagster_data_lake/schedules/schedule.py
@@ -2,7 +2,7 @@ from dagster import schedule
 
 from dagster_data_lake.jobs.pipe_api import pipe_api_job
 
-
+# A change so that I can create a PR and trigger a build
 @schedule(cron_schedule="0 * * * *", job=pipe_api_job, execution_timezone="US/Central")
 def hourly_schedule(_) -> None:
     return {}
